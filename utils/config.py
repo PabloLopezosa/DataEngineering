0f1241c6bb65e24d7a01e5bc325369f80b7084ab
Pablo Lopezosa <97469174+PabloLopezosa@users.noreply.github.com>
2022-05-12 10:04:39 +0200

    Add files via upload

---
+++ utils/config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = "TMgW4cECe234i20945u3sRw*4!rP2352!$!$%&7ñPKNJNDFWOM9qnnf$B"
+MODEL = "data/model/model.pkl"
+HOST = "database-poc.ceeicqti4fmz.eu-west-3.rds.amazonaws.com"
+USER = "adminpoc"
+PASSWORD = "adminpoc"
